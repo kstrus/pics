c4022a5006ba277e2ac58a88a698b77d5ce04e5f
kstrus <katarzyna.anna.strus@gmail.com>
2018-12-03 20:01:25 +0100

    Move App component to 'components' folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-    render() {
-        return <div>Test</div>;
-    }
-}
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
